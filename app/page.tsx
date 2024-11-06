f22c09629e338ff9641dbc94d623adbdc57c5ce0
Gabriel Floriano <gabriel.floriano@dmsoft.com.br>
2024-11-06 13:52:19 -0300

    feat: add authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-const Home = () => {
-  return <h1>Hello word</h1>;
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return <UserButton showName></UserButton>;
 };
 
 export default Home;
